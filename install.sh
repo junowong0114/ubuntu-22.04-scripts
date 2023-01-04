742e88d4e8a9de316b8c279d982fd777eb4d6762
Juno Wong <junowong2012@gmail.com>
2023-01-04 12:40:47 +0800

    default install lts/hydrogen node

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -eo pipefail
+set -x
 
 # Use absolute path
 path=$(readlink -f "${BASH_SOURCE:-$0}")
@@ -12,6 +13,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install node
+nvm install lts/hydrogen
+nvm alias default lts/hydrogen
 
 # configure git
 git config --global user.email "$GIT_USER_EMAIL"
